41eea5b65cdbfef5eb9b24d035a3f19e2307e8ae
castromaxi <mc.castro@alumno.etec.um.edu.ar>
2024-06-07 12:38:30 -0300

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get("/saludo/:nombre", (req, res ) => {
+  const nombre = req.params.nombre
+  res.send(`hola ${nombre}`);
+});
+
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
 });
